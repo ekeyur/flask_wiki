776e18a87d9c517c289399ca10251b0b4495093b
Keyur Patel <ekeyur@gmail.com>
2016-10-17 16:56:55 -0400

    Completed stage 1

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, redirect, request, session, flash
-from wiki_linkify import wiki_linkify
-import pg, time
+from dotenv import load_dotenv, find_dotenv
 
-app = Flask("My Wiki")
-db = pg.DB(dbname='wiki_db')
+load_dotenv(find_dotenv())
+
+from flask import Flask, render_template, redirect, request, session, flash
+
+from wiki_linkify import wiki_linkify
+
+import pg, time, os
+
+
+tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask('Wiki', template_folder=tmp_dir)
+
+db = pg.DB(
+    dbname=os.environ.get('PG_DBNAME'),
+    host=os.environ.get('PG_HOST'),
+    user=os.environ.get('PG_USERNAME'),
+    passwd=os.environ.get('PG_PASSWORD')
+)
 
 app.secret_key = 'keyur12345'
 
